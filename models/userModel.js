cf76003c01b666827087527b2532c7fb1b8814ff
itsKiran55 <kiranraja557@gmail.com>
2024-09-19 16:08:02 +0500

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    employeeID: {
+    employeeId: {
         type: Number,
         required: true,
         unique: true,
@@ -55,5 +55,5 @@ const userSchema = new mongoose.Schema({
     refreshToken: String,
 }, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-export default User;
+const User = mongoose.models.User  || mongoose.model('User', userSchema);
+export default User
